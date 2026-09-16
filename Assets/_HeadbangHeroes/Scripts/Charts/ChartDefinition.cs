a0004f41054504f2bb6f5eb2cf1d9c630141d610
Gianluca Pizzuka Pizzamiglio <39455851+PizzukaTorph@users.noreply.github.com>
2026-09-16 11:20:17 +0200

    feat: add horizontal and vertical Classic Bang directions

--- Assets/_HeadbangHeroes/Scripts/Charts/ChartDefinition.cs
+++ Assets/_HeadbangHeroes/Scripts/Charts/ChartDefinition.cs
@@ -4,7 +4,43 @@ using UnityEngine;
 
 namespace HeadbangHeroes.Charts
 {
-    public enum BangDirection { Left = -1, Right = 1 }
+    public enum BangDirection
+    {
+        Left = 0,
+        Right = 1,
+        Up = 2,
+        Down = 3
+    }
+
+    public enum BangAxis
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public static class BangDirectionExtensions
+    {
+        public static BangAxis Axis(this BangDirection direction)
+            => direction == BangDirection.Left || direction == BangDirection.Right
+                ? BangAxis.Horizontal
+                : BangAxis.Vertical;
+
+        /// <summary>
+        /// Direction the head launches after this inversion point.
+        /// LEFT launches RIGHT, RIGHT launches LEFT, UP launches DOWN, DOWN launches UP.
+        /// </summary>
+        public static Vector2 LaunchVector(this BangDirection direction)
+        {
+            switch (direction)
+            {
+                case BangDirection.Left: return Vector2.right;
+                case BangDirection.Right: return Vector2.left;
+                case BangDirection.Up: return Vector2.down;
+                case BangDirection.Down: return Vector2.up;
+                default: return Vector2.zero;
+            }
+        }
+    }
 
     [Serializable]
     public struct ChartEvent
